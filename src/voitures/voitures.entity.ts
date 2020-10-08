9d8bb526e5222d95cca0eee6de81c4b99e8c917d
pauffret <pierreauffret@free.fr>
2020-10-08 18:24:39 +0200

    Modif nom table entité voitures

--- src/voitures/voitures.entity.ts
+++ src/voitures/voitures.entity.ts
@@ -1,7 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-//pass the name of table inside @Entity() i.e "users", if you don't pass table name it will create "users_entity" table by default
-@Entity('users')
+@Entity('voiture')
 export class VoituresEntity {
     @PrimaryGeneratedColumn()
     id: number;
